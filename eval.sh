a17edadfd4a78c6da55a8894179001ee9f8a372a
danjuan-77 <youngertutu@gmail.com>
2025-05-05 20:56:21 +0800

    update

--- eval.sh
+++ eval.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=3
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LAQA
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LAQA
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LIQA
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LIQA
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LVQA
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LVQA
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_2/MAIC
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_2/MAIC
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_2/MVIC
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_2/MVIC
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVH
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVH
 
 python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVL
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVM
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVM
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVR
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVR
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/VAH
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/VAH
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/VAR
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/VAR
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVC
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVC
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVLG
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVLG
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVQA
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVQA
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVLG
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVLG
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVQA
+# python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVQA
 
 # nohup bash eval.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_minicpm-o_gpu2_$(date +%Y%m%d%H%M%S).log 2>&1 &
