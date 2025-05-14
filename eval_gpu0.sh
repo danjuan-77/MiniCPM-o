387979774dd4c23cce671a363ac58a1c6e002de6
danjuan-77 <youngertutu@gmail.com>
2025-05-14 16:17:18 +0800

    update

--- eval_gpu0.sh
+++ eval_gpu0.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 
-python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVH
+python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_6/AVSQA_av
+
+python eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_6/AVSQA_v
+
 
 # nohup bash eval_gpu0.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_minicpm-o_unimodal_gpu0_$(date +%Y%m%d%H%M%S).log 2>&1 &
